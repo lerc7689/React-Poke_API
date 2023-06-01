d0f1ad1ec9a55a659c563188a476d5c27451ae8d
Luis Ramirez <lerc7689@hotmail.com>
2023-06-01 15:23:16 -0400

    pointer

--- src/pages/PageNotFound/PageNotFound.jsx
+++ src/pages/PageNotFound/PageNotFound.jsx
@@ -3,7 +3,6 @@ import "./PageNotFound.css"
 const PageNotFound = () =>{
     return(<>
             <div className="pageNotFound">
-                {/* <h1>Error 404 : Page Not Found</h1> */}
                 <div className="imgContainerNotFound">
                     <img src={imgNotFound} alt="" />
                 </div>
