4a933960407e47e009792ad310da708b6a77c21a
Luis Ramirez <lerc7689@hotmail.com>
2023-06-02 00:56:23 -0400

    pokemon detail style

--- src/pages/PokedexId/Pokedex.jsx
+++ src/pages/PokedexId/Pokedex.jsx
@@ -34,10 +34,32 @@ const PokedexId = () =>{
             </div>
             <div className="allInfoContainer">
                 <h1 className="detailPokemonName"><b>#{pokemonId} {pokemonDetail?.name[0].toUpperCase() + pokemonDetail?.name.slice(1)}</b></h1>
-                <p className="types"><b className="typeTitle">Type:</b> {pokemonDetail?.types.map(t => <p className="singleType">{t.type.name}</p>)} </p> 
-                <p className="abilities"><b className="abilityTitle">Abilities:</b> {pokemonDetail?.abilities.map(a => <p className="singleAbility">{a.ability.name}</p> )} </p>
-                <p className="stats"><b className="statTitle">Stats: </b>{pokemonDetail?.stats.map(s => <p className="singleStat">{ s.stat.name + " : " + s.base_stat} </p>)}</p>
-                <p className="movements"><b className="moveTitle">Movements: </b>{pokemonDetail?.moves.map(m => <p className="singleMove">{ m.move.name + " "}</p>)}</p>
+                <div className="type_ability_stat_Container">
+                    <div className="type_ability_container">
+                        <b className="typeTitle">Type</b> 
+                        <p className="types">{pokemonDetail?.types.map(t => <p className="singleType">{t.type.name}</p>)} </p> 
+                        <b className="abilityTitle">Abilities</b>
+                        <p className="abilities"> {pokemonDetail?.abilities.map(a => <p className="singleAbility">{a.ability.name}</p> )} </p>
+                        
+                    </div>
+                    <div className="stats">
+                        <b className="statTitle">Stats </b>
+                        {pokemonDetail?.stats.map(s => 
+
+                            <div className="progress-bar"><div><div className="statNameDiv"><b>{ s.stat.name[0].toUpperCase() + s.stat.name.slice(1)}</b></div> <div className="statpropDiv">{s.base_stat}</div></div>
+                                <div className="progress" style={({width:s.base_stat*2})}></div>
+                            </div>
+                        )}
+                    </div>
+                        
+                </div>
+                    <div className="movementsContainer">
+                        <b className="moveTitle">Movements </b>
+                        <div className="movements">
+
+                            {pokemonDetail?.moves.map(m => <p className="singleMove">{ m.move.name + " "}</p>)}
+                        </div>
+                    </div>
             </div>
         </div>
     </>)
